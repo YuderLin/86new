d38df009d9fc095d5577238f96dfebf4acd4df68
YuderLin <91525008+YuderLin@users.noreply.github.com>
2021-12-12 11:39:18 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+//kurena
+$('.Kurena').click(function(event) {
+	$('.Kurena').css('background-image', 'url(img/Kurena-active.png)');
+});
+$('.Kurena').mouseover(function(event) {
+	  $('.Kurena').css('background-image', 'url(img/Kurena-hover.png)');
+  });
+
+$('.Kurena').mouseleave(function(ebent){
+	$('.Kurena').css('background-image','url(img/Kurena_1.png)')
+});
+
+//anju
+$('.Anju').click(function(event) {
+	$('.Anju').css('background-image', 'url(img/Anju-active.png)');
+});
+$('.Anju').mouseover(function(event) {
+	  $('.Anju').css('background-image', 'url(img/Anju-hover.png)');
+  });
+
+$('.Anju').mouseleave(function(ebent){
+	$('.Anju').css('background-image','url(img/Anju_1.png)')
+});
+
+//Lena
+$('.Lena').click(function(event) {
+	$('.Lena').css('background-image', 'url(img/Lena-active.png)');
+});
+$('.Lena').mouseover(function(event) {
+	  $('.Lena').css('background-image', 'url(img/Lena-hover.png)');
+  });
+
+$('.Lena').mouseleave(function(ebent){
+	$('.Lena').css('background-image','url(img/Lena_1.png)')
+});
+
+//Shin
+$('.Shin').click(function(event) {
+	$('.Shin').css('background-image', 'url(img/Shin-active.png)');
+});
+$('.Shin').mouseover(function(event) {
+	  $('.Shin').css('background-image', 'url(img/Shin-hover.png)');
+  });
+
+$('.Shin').mouseleave(function(ebent){
+	$('.Shin').css('background-image','url(img/Shin_1.png)')
+});
+
+//Raiden
+$('.Raiden').click(function(event) {
+	$('.Raiden').css('background-image', 'url(img/Raiden-active.png)');
+});
+$('.Raiden').mouseover(function(event) {
+	  $('.Raiden').css('background-image', 'url(img/Raiden-hover.png)');
+  });
+
+$('.Raiden').mouseleave(function(ebent){
+	$('.Raiden').css('background-image','url(img/Raiden_1.png)')
+});
+
+//Theo
+$('.Theo').click(function(event) {
+	$('.Theo').css('background-image', 'url(img/Theo-active.png)');
+});
+$('.Theo').mouseover(function(event) {
+	  $('.Theo').css('background-image', 'url(img/Theo-hover.png)');
+  });
+
+$('.Theo').mouseleave(function(ebent){
+	$('.Theo').css('background-image','url(img/Theo_1.png)')
+});
+
+//+
+$('.addmore').mouseover(function(event){
+	$('.addmore').css('color', '#F9C2C2')
+});
+$('.addmore').click(function(event){
+	$('.addmore').css('color', '#440707')
+});
+$('.addmore').mouseleave(function(event){
+	$('.addmore').css('color', '#FFFFFF')
+});
